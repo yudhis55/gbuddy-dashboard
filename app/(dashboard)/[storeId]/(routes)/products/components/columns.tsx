65b5b87bf4d474e3c7bc0d1c8997519564fa6a9e
Andr Fla <im.andrflaa26@gmail.com>
2023-12-10 20:46:40 +0700

    bismillah final

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { CellAction } from "./cell-action"
+
+export type ProductColumn = {
+  id: string
+  name: string;
+  price: string;
+  category: string;
+  createdAt: string;
+  isFeatured: boolean;
+  isArchived: boolean;
+}
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Archived",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />
+  },
+];
---
+++ app/api/[storeId]/products/route.ts
@@ -0,0 +1,116 @@
+import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
+
+import prismadb from '@/lib/prismadb';
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    const body = await req.json();
+
+    const { name, price, description, categoryId, images, isFeatured, isArchived } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!description) {
+      return new NextResponse("Description is required", { status: 400 });
+    }
+
+    if (!images || !images.length) {
+      return new NextResponse("Images are required", { status: 400 });
+    }
+
+    if (!price) {
+      return new NextResponse("Price is required", { status: 400 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse("Category id is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId
+      }
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 405 });
+    }
+
+    const product = await prismadb.product.create({
+      data: {
+        name,
+        description,
+        price,
+        isFeatured,
+        isArchived,
+        categoryId,
+        storeId: params.storeId,
+        images: {
+          createMany: {
+            data: [
+              ...images.map((image: { url: string }) => image),
+            ],
+          },
+        },
+      },
+    });
+  
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log('[PRODUCTS_POST]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } },
+) {
+  try {
+    const { searchParams } = new URL(req.url)
+    const categoryId = searchParams.get('categoryId') || undefined;
+    const isFeatured = searchParams.get('isFeatured');
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    const products = await prismadb.product.findMany({
+      where: {
+        storeId: params.storeId,
+        categoryId,
+        isFeatured: isFeatured ? true : undefined,
+        isArchived: false,
+      },
+      include: {
+        images: true,
+        category: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      }
+    });
+  
+    return NextResponse.json(products);
+  } catch (error) {
+    console.log('[PRODUCTS_GET]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
